6a917acd23083c6cad6d6b380cf32873d34e64c2
Igor Diniz <igordinizsilveira@hotmail.com>
2023-09-05 22:31:56 -0400

    Criado projeto web Jogo da Velha

---
+++ scripts/game.js
@@ -0,0 +1,65 @@
+let board = ["","","","","","","","",""];
+let turn = 0;
+let symbols = ["o", "x"];
+let gameOver = false;
+let winner;
+
+let winStates = 
+[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+function handleMove(position)
+{
+    if(gameOver)
+    {
+        return;
+    }
+
+    if(board[position] == '')
+    {
+        board[position] = symbols[turn];
+
+        gameOver = isWin();
+
+        if(gameOver == false)
+        {
+            turn = (turn == 0) ? 1 : 0;
+        }
+    }
+
+    setWinner();
+
+    return gameOver;
+}
+
+function isWin()
+{
+    for(let i = 0; i < winStates.length; i++)
+    {
+        let seq = winStates[i];
+
+        let pos1 = seq[0];
+        let pos2 = seq[1];
+        let pos3 = seq[2];
+
+        if(board[pos1] == board[pos2] && board[pos1] == board[pos3] && board[pos1] != "")
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function setWinner()
+{
+    winner = (gameOver && turn == 0) ? "🛡️" : "⚔️";
+}
